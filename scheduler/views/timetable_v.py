058e0653f3bf739e54bee179bc5299979b624fe0
sang <mim3431@naver.com>
2017-12-20 17:42:08 +0900

    add alert message : 시간표 구성 실패시

--- scheduler/views/timetable_v.py
+++ scheduler/views/timetable_v.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect
 from django.views.generic import ListView
+from django.contrib import messages
 
 from scheduler.models.company_m import CompanyInfo
 from scheduler.models.subject_m import SubjectInfo
 from scheduler.solver.function_of_timetable import assign_to_different_time
-from scheduler.string_name import BEFORE_SUBJECT, AFTER_SUBJECT, ID, START_TIME, END_TIME
+from scheduler.string_name import BEFORE_SUBJECT, AFTER_SUBJECT, ID, START_TIME, END_TIME, NAME
 from ..tables import TimetableTable, SubjectTable
 from ..views.common_v import request_table_config
 
@@ -41,7 +42,6 @@ class TimetableListView(ListView):
 
 
 def update_timetable(request, company_id):
-    selected_subject_list = []
     if request.method == 'POST':
         selected_subject_list = request.POST.getlist('select_subject')
         timetable_type = request.POST.get('timetable_type')
@@ -52,8 +52,6 @@ def update_timetable(request, company_id):
         if select_all is not None:
             subject_list = SubjectInfo.objects.filter(company_id=company_id)
             for subject in subject_list:
-                subject.set_timetable = True
-                subject.save()
                 if subject.week is None:
                     group_by_week[7].append(subject.get_model())
                 else:
@@ -61,19 +59,28 @@ def update_timetable(request, company_id):
         else:
             for index in selected_subject_list:
                 subject = SubjectInfo.objects.get(id=index)
-                subject.set_timetable = True
-                subject.save()
                 if subject.week is None:
                     group_by_week[7].append(subject.get_model())
                 else:
                     group_by_week[subject.week].append(subject.get_model())
 
+        warning_subject = "\n"
+        display_warning = False
+
         for week_index, timetable in enumerate(group_by_week):
             assign_to_different_time(timetable, week_index)
+
             for data in timetable:
                 subject = SubjectInfo.objects.get(id=data.get(ID))
-                subject.start_time = data.get(START_TIME)
-                subject.end_time = data.get(END_TIME)
-                subject.save()
-
+                if data.get(START_TIME) is not None:
+                    subject.set_timetable = True
+                    subject.start_time = data.get(START_TIME)
+                    subject.end_time = data.get(END_TIME)
+                    subject.save()
+                else:
+                    display_warning = True
+                    warning_subject = warning_subject + data.get(NAME) + ",  "
+        if display_warning:
+            warning_subject = warning_subject[0:warning_subject.__len__() - 3]
+            messages.warning(request, '다음 선택한 과목은 해당 요일 혹은 강의실에 배정이 어렵습니다 : ' + warning_subject)
     return redirect('timetable', company_id=company_id)
